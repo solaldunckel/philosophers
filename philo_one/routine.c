f051141885e66a6b3fb68461d882f7dfa05add74
solaldunckel <solaldunckel@gmail.com>
2020-07-07 15:08:20 +0200

    try fix philo_one VM

--- philo_one/routine.c
+++ philo_one/routine.c
@@ -6,7 +6,7 @@
 /*   By: sdunckel <sdunckel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 17:22:09 by sdunckel          #+#    #+#             */
-/*   Updated: 2020/07/05 23:26:17 by sdunckel         ###   ########.fr       */
+/*   Updated: 2020/07/07 15:07:43 by sdunckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,26 @@ void	real_sleep(int n)
 	time_t start;
 
 	start = get_time();
-	while (1)
-	{
-		if (get_time() - start >= n)
-			break ;
-		usleep(1000);
-	}
+	while ((get_time() - start) < n)
+		usleep(100);
 }
 
 void	eat(t_philo *philo)
 {
-	while (1)
-	{
-		if (!philo->options->forks_n[philo->left]
-			&& !philo->options->forks_n[philo->right])
-		{
-			philo->options->forks_n[philo->left] = 1;
-			philo->options->forks_n[philo->right] = 1;
-			pthread_mutex_lock(&philo->options->forks[philo->left]);
-			pthread_mutex_lock(&philo->options->forks[philo->right]);
-			state_msg(philo, "has taken a fork", philo->options->start_time);
-			state_msg(philo, "has taken a fork", philo->options->start_time);
-			break ;
-		}
-	}
+  if (philo->pos % 2 == 0)
+  {
+    pthread_mutex_lock(&philo->options->forks[philo->left]);
+    state_msg(philo, "has taken a fork", philo->options->start_time);
+    pthread_mutex_lock(&philo->options->forks[philo->right]);
+    state_msg(philo, "has taken a fork", philo->options->start_time);
+  }
+  else
+  {
+    pthread_mutex_lock(&philo->options->forks[philo->right]);
+    state_msg(philo, "has taken a fork", philo->options->start_time);
+    pthread_mutex_lock(&philo->options->forks[philo->left]);
+    state_msg(philo, "has taken a fork", philo->options->start_time);
+  }
 	pthread_mutex_lock(&philo->eating);
 	state_msg(philo, "is eating", philo->options->start_time);
 	philo->last_eat = get_time();
@@ -48,14 +44,11 @@ void	eat(t_philo *philo)
 	pthread_mutex_unlock(&philo->eating);
 	pthread_mutex_unlock(&philo->options->forks[philo->left]);
 	pthread_mutex_unlock(&philo->options->forks[philo->right]);
-	philo->options->forks_n[philo->left] = 0;
-	philo->options->forks_n[philo->right] = 0;
 }
 
 void	philo_routine(t_philo *philo)
 {
 	int		eat_count;
-	time_t	time;
 
 	eat_count = 0;
 	while (1)
@@ -69,7 +62,6 @@ void	philo_routine(t_philo *philo)
 			return ;
 		}
 		state_msg(philo, "is sleeping", philo->options->start_time);
-		time = get_time();
 		real_sleep(philo->options->time_to_sleep);
 		state_msg(philo, "is thinking", philo->options->start_time);
 	}
